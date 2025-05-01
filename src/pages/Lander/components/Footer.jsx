272264612eec5d7153adbeee54895b4e17fc0dfe
saiVardhan1802 <svchalaka@gmail.com>
2025-05-01 22:37:32 +0530

    fixing landing page styles

--- src/pages/Lander/components/Footer.jsx
+++ src/pages/Lander/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './styles/Footer.module.css'
+import PlexifyIcon from '../../../assets/global/PlexifyIcon.svg';
+import { discordIcon, figmaIcon, instaIcon, linkedInIcon, mailIcon, twitterIcon, youTubeIcon } from '../../../assets/lander/images';
 
 const Footer = () => {
     const product = [
@@ -35,54 +37,81 @@ const Footer = () => {
     ];
 
     const company = ["Careers"]
-  return (
-    <div className={styles.footer}>
-      <div className={styles.column}>
-        <h4 className={styles.title}>Product</h4>
-        <ul className={styles.list}>
-          {product.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </div>
-
-      <div className={styles.column}>
-        <h4 className={styles.title}>Why Primer</h4>
-        <ul className={styles.list}>
-          {primer.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </div>
-
-      <div className={styles.column}>
-        <h4 className={styles.title}>Developers</h4>
-        <ul className={styles.list}>
-          {developers.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </div>
-
-      <div className={styles.column}>
-        <h4 className={styles.title}>Resources</h4>
-        <ul className={styles.list}>
-          {resources.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </div>
-
-      <div className={styles.column}>
-        <h4 className={styles.title}>Company</h4>
-        <ul className={styles.list}>
-          {company.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+    return (
+        <div className={styles.footerContainer}>
+            <div style={{
+                display: 'flex',
+                color: '#184E7F',
+                marginTop: '10%',
+                fontSize: '2rem'
+            }}>
+                <img src={PlexifyIcon} alt="plexify icon" style={{
+                    width: '30%',
+                    height: 'auto'
+                }} />
+                <h1>Hubly</h1>
+            </div>
+            <div className={styles.footer}>
+                <div className={styles.column}>
+                    <h4 className={styles.title}>Product</h4>
+                    <ul className={styles.list}>
+                        {product.map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h4 className={styles.title}>Why Primer</h4>
+                    <ul className={styles.list}>
+                        {primer.map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h4 className={styles.title}>Developers</h4>
+                    <ul className={styles.list}>
+                        {developers.map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h4 className={styles.title}>Resources</h4>
+                    <ul className={styles.list}>
+                        {resources.map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h4 className={styles.title}>Company</h4>
+                    <ul className={styles.list}>
+                        {company.map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div style={{
+                display: 'flex',
+                position: 'absolute',
+                bottom: '5%',
+                right: '2%',
+                gap:
+                '1em',
+                alignItems: 'center'
+            }}>
+                <img src={mailIcon} alt="" />
+                <img src={linkedInIcon} alt="" />
+                <img src={twitterIcon} alt="" />
+                <img src={youTubeIcon} alt="" />
+                <img src={discordIcon} alt="" />
+                <img src={figmaIcon} alt="" />
+                <img src={instaIcon} alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Footer
--- src/pages/Lander/components/HeroLeft.jsx
+++ src/pages/Lander/components/HeroLeft.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { LuArrowRight } from "react-icons/lu";
 import { BsPlayCircle } from "react-icons/bs";
+import { useNavigate } from 'react-router-dom';
 
 const HeroLeft = () => {
+  const navigate = useNavigate();
     const buttonStyles = {
         display: 'flex',
         gap: '0.5em',
         alignItems: 'center',
-        padding: '1em'
+        padding: '1em',
+        borderRadius: '8px'
     }
   return (
     <div style={{
@@ -16,18 +19,26 @@ const HeroLeft = () => {
         flexDirection: 'column',
         gap: '1em',
         color: '#30404D',
-        width: '40%'
+        width: '40%',
+        marginTop: '5%'
     }}>
-      <h2>Grow Your Business Faster with Hubly CRM</h2>
+      <h2 style={{
+        fontSize: '2.5rem'
+      }}>Grow Your Business Faster with Hubly CRM</h2>
       <p>Manage leads, automate workflows, and close deals effortlessly—all in one powerful platform.</p>
-      <div>
-        <button style={{
-            ...buttonStyles
+      <div style={{
+        display: 'flex',
+        gap: '1em'
+      }}>
+        <button type='button' onClick={() => navigate('/sign-up')} style={{
+            ...buttonStyles,
+            backgroundColor: '#244779',
+            color: 'white'
         }}>
             <p>Get Started</p>
             <LuArrowRight />
         </button>
-        <button style={{
+        <button type='button' style={{
             ...buttonStyles
         }}>
             <BsPlayCircle />
